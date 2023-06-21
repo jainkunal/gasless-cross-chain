f8fce163c3d34ed8baaf3f38a2c7b1bb4fea6067
Kunal Jain <kunal@joinwido.com>
2023-06-21 14:46:05 +0530

    Calling paymaster API

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import './App.css'
 import { useState, useEffect, useRef } from 'react'
 import SocialLogin from "@biconomy/web3-auth"
+
 import { ChainId } from "@biconomy/core-types";
 import { ethers } from 'ethers'
 import SmartAccount from "@biconomy/smart-account";
-// import { WidoWidget } from "wido-widget";
-import { getSupportedTokens } from "wido";
+import { approve } from "wido";
 
 import abi from "./erc20abi.json"
-import { StackupPaymasterAPI } from './StackupPaymasterAPI';
+import { WidoPaymasterAPI } from './WidoPaymasterAPI';
+
+
+const USDC_POLYGON = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
+const VAULT_ARBITRUM = "0x562Ae83d17590d9681D5445EcfC0F56517e49f24";
+
 
 function App() {
   const [smartAccount, setSmartAccount] = useState<SmartAccount | null>(null)
   const [interval, enableInterval] = useState(false)
   const sdkRef = useRef<SocialLogin | null>(null)
   const [loading, setLoading] = useState<boolean>(false)
-  const [, setProvider] = useState<any>(null)
+  const [provider, setProvider] = useState<any>(null)
 
   // const [fromTokens, setFromTokens] = useState([]);
   // const [toTokens, setToTokens] = useState([]);
@@ -46,12 +51,13 @@ function App() {
       const socialLoginSDK = new SocialLogin()
       const signature1 = await socialLoginSDK.whitelistUrl('http://127.0.0.1:5173/')
       await socialLoginSDK.init({
-        chainId: ethers.utils.hexValue(ChainId.POLYGON_MUMBAI).toString(),
-        network: "testnet",
+        chainId: ethers.utils.hexValue(ChainId.POLYGON_MAINNET).toString(),
+        network: "mainnet",
         whitelistUrls: {
           'http://127.0.0.1:5173/': signature1,
         }
       })
+      console.log(socialLoginSDK);
       sdkRef.current = socialLoginSDK
     }
     if (!sdkRef.current.provider) {
@@ -64,20 +70,23 @@ function App() {
 
   async function setupSmartAccount() {
     if (!sdkRef?.current?.provider) return
+    console.log("setting up smart account...")
     sdkRef.current.hideWallet()
     setLoading(true)
     const web3Provider = new ethers.providers.Web3Provider(
       sdkRef.current.provider
     )
     setProvider(web3Provider)
+
+    console.log(web3Provider);
     try {
       const smartAccount = new SmartAccount(web3Provider, {
-        activeNetworkId: ChainId.POLYGON_MUMBAI,
-        supportedNetworksIds: [ChainId.POLYGON_MUMBAI, ChainId.GOERLI],
+        activeNetworkId: ChainId.POLYGON_MAINNET,
+        supportedNetworksIds: [ChainId.POLYGON_MAINNET, ChainId.ARBITRUM_ONE_MAINNET],
         networkConfig: [
           {
-            chainId: ChainId.POLYGON_MUMBAI,
-            // customPaymasterAPI: new StackupPaymasterAPI(),
+            chainId: ChainId.POLYGON_MAINNET,
+            customPaymasterAPI: new WidoPaymasterAPI(),
             dappAPIKey: "oEHNi7Cc9.f1b032c0-b868-43e9-aef0-27b6ec8b1b24",
           },
         ],
@@ -101,29 +110,71 @@ function App() {
     enableInterval(false)
   }
 
-  async function sendERC20() {
+  // async function sendERC20() {
+  //   if (!smartAccount) return
+
+  //   const sas = await smartAccount.getSmartAccountState();
+  //   console.log(`Entrypoint address: ${sas.entryPointAddress}`);
+
+  //   const contract = new ethers.Contract(
+  //     "0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1",
+  //     abi,
+  //     provider,
+  //   )
+  //   const transferTx = await contract.populateTransaction.transfer("0x116F609A03425c210cD28391497e7b03D31fC051", ethers.utils.parseEther('0.01'))
+
+  //   const txResponse = await smartAccount.sendTransaction({
+  //     transaction: {
+  //       to: '0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1',
+  //       data: transferTx.data,
+  //       value: 1
+  //     },
+  //     chainId: ChainId.POLYGON_MUMBAI,
+  //   })
+
+  //   const txHash = await txResponse.wait();
+  //   console.log(txHash)
+  // }
+
+  async function depositPolygonUSDCToArbitrumVault() {
     if (!smartAccount) return
 
     const sas = await smartAccount.getSmartAccountState();
     console.log(`Entrypoint address: ${sas.entryPointAddress}`);
 
-    const contract = new ethers.Contract(
-      "0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1",
-      abi,
-      provider,
-    )
-    const transferTx = await contract.populateTransaction.transfer("0x116F609A03425c210cD28391497e7b03D31fC051", ethers.utils.parseEther('0.01'))
+    const amount = ethers.utils.parseUnits('10', 6);
+    console.log(amount);
 
-    const txResponse = await smartAccount.sendTransaction({
-      transaction: {
-        to: '0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1',
-        data: transferTx.data,
-      },
-      chainId: ChainId.POLYGON_MUMBAI,
-    })
+    // Get approve transaction
+    const { to: approveTo, data: approveCalldata } = await approve({
+      chainId: ChainId.POLYGON_MAINNET,
+      fromToken: USDC_POLYGON,
+      // TODO: Fix the SDK
+      // toChainId: ChainId.ARBITRUM_ONE_MAINNET,
+      toToken: VAULT_ARBITRUM,
+      amount: amount.toString(),
+    });
+    console.log(approveTo)
+    console.log(approveCalldata)
+
+    // Get deposit transaction
+
+
+    const txResponse = await smartAccount.sendTransactionBatch({
+      transactions: [
+        {
+          to: approveTo,
+          data: approveCalldata,
+        },
+      ],
+      chainId: ChainId.POLYGON_MAINNET,
+    });
 
     const txHash = await txResponse.wait();
     console.log(txHash)
+  }
+
+  async function withdrawArbitrumVaultToPolygonUSDC() {
   }
 
   return (
@@ -146,7 +197,9 @@ function App() {
                 <button onClick={logout}>Logout</button>
               </div>
               <div>
-                <button onClick={sendERC20}>Send ERC20</button>
+                <button onClick={depositPolygonUSDCToArbitrumVault}>Deposit Polygon USDC to Arbitrum Vault</button>
+                <button onClick={withdrawArbitrumVaultToPolygonUSDC}>Withdraw Arbitrum Vault to Polygon USDC</button>
+                {/* <button onClick={sendERC20}>Send ERC20</button> */}
                 {/* <WidoWidget ethProvider={provider} onSwitchChain={(p) => {
                   console.log('switching chain');
                   console.log(p);
---
+++ src/WidoPaymasterAPI.ts
@@ -0,0 +1,78 @@
+import {
+  IPaymasterAPI,
+  PaymasterServiceDataType,
+  UserOperation,
+} from "@biconomy/core-types";
+import { resolveProperties } from "@ethersproject/properties";
+
+export class WidoPaymasterAPI implements IPaymasterAPI {
+  async getPaymasterAndData(
+    userOp: Partial<UserOperation>,
+    paymasterServiceData?: PaymasterServiceDataType | undefined
+  ): Promise<string> {
+    console.log("In WidoPaymasterAPI.getPaymasterAndData");
+    userOp = await resolveProperties(userOp);
+
+    userOp.nonce = Number(userOp.nonce);
+    userOp.callGasLimit = Number(userOp.callGasLimit);
+    userOp.verificationGasLimit = Number(userOp.verificationGasLimit);
+    userOp.maxFeePerGas = Number(userOp.maxFeePerGas);
+    userOp.maxPriorityFeePerGas = Number(userOp.maxPriorityFeePerGas);
+    userOp.preVerificationGas = Number(userOp.preVerificationGas);
+    userOp.signature = "0x";
+    userOp.paymasterAndData = "0x";
+
+    console.log(
+      JSON.stringify({
+        sender: userOp.sender, // address
+        nonce: userOp.nonce, // uint256
+        initCode: userOp.initCode, // bytes
+        callData: userOp.callData, // bytes
+        callGasLimit: userOp.callGasLimit, // uint256
+        verificationGasLimit: userOp.verificationGasLimit, // uint256
+        preVerificationGas: userOp.preVerificationGas, // uint256
+        maxFeePerGas: userOp.maxFeePerGas, // uint256
+        maxPriorityFeePerGas: userOp.maxPriorityFeePerGas, // uint256
+        paymasterAndData: userOp.paymasterAndData, // bytes
+        signature: userOp.signature,
+      })
+    );
+    const res = await fetch("http://localhost:8080/paymaster", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        sender: userOp.sender, // address
+        nonce: userOp.nonce, // uint256
+        initCode: userOp.initCode, // bytes
+        callData: userOp.callData, // bytes
+        callGasLimit: userOp.callGasLimit, // uint256
+        verificationGasLimit: userOp.verificationGasLimit, // uint256
+        preVerificationGas: userOp.preVerificationGas, // uint256
+        maxFeePerGas: userOp.maxFeePerGas, // uint256
+        maxPriorityFeePerGas: userOp.maxPriorityFeePerGas, // uint256
+        paymasterAndData: userOp.paymasterAndData, // bytes
+        signature: userOp.signature,
+      }),
+    });
+
+    if (!res.ok) {
+      throw new Error("Error in WidoPaymasterAPI");
+    }
+
+    const { paymasterAndData } = await res.json();
+
+    if (
+      paymasterAndData !== undefined &&
+      paymasterAndData !== "" &&
+      paymasterAndData !== "0x"
+    ) {
+      console.log("Wido is paying for the tx");
+    }
+
+    return paymasterAndData;
+  }
+}
